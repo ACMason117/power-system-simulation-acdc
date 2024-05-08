4f9dc45e354d4cc170142c84e283d0c0bfbce5e5
BrittElbers <167319376+BrittElbers@users.noreply.github.com>
2024-05-08 10:46:20 +0200

    Test down stream edges

---
+++ tests/test_graph.py
@@ -0,0 +1,27 @@
+#import numpy as np
+import pytest
+import assignment_1.graph_processing as tp
+
+def test_downstream_vertices():
+    vertex_ids = [0, 1, 2, 3, 4, 5]  # All unique vertex ids
+    edge_ids = [1, 2, 3, 4, 5]  # All unique edge ids
+    edge_vertex_id_pairs = [(0, 1), (0, 2), (2, 3), (3, 4), (4, 5)]
+    edge_enabled = [True, True, True, True, True]
+    source_vertex_id = 0
+
+    test3 = tp.GraphProcessor(
+        vertex_ids=vertex_ids,
+        edge_ids=edge_ids,
+        edge_vertex_id_pairs=edge_vertex_id_pairs,
+        edge_enabled=edge_enabled,
+        source_vertex_id=source_vertex_id,
+    )
+
+    downstream_vertices = test3.find_downstream_vertices(0)
+    assert downstream_vertices ==[1, 2]
+
+    downstream_vertices = test3.find_downstream_vertices(1)
+    assert downstream_vertices ==[]
+
+    downstream_vertices = test3.find_downstream_vertices(2)
+    assert downstream_vertices == [3]
