8054b2a6b9c9517dce6d64842fa79374d1e66cfe
gijsvanderburgh <165763444+gijsvanderburgh@users.noreply.github.com>
2024-05-15 17:20:24 +0200

    json file support
    .json files can now be loaded also they can be processed. Also I have created a test case to test the file.

--- src/power_system_simulation/power_flow_processing.py
+++ src/power_system_simulation/power_flow_processing.py
@@ -1,27 +1,42 @@
+# power_flow_processing.py
+"""
+In this file the processing of the power system should be done. Power system can be given in a different test file. 
+"""
+
 import json
 import pprint
-import warnings
+
 from pandas import DataFrame
 from power_grid_model import PowerGridModel
 from power_grid_model.utils import json_deserialize, json_serialize
+
 
 class PowerFlow:
     """
     General documentation of this class.
     You need to describe the purpose of this class and the functions in it.
-    We are initializing the data here. 
+    We are initializing the data here.
     """
-    def __init__(self):
+
+    def __init__(self, data=None):
         # Load data upon instantiation
-        self.load_data()
+        if data is not None:
+            self.data = data
+        else:
+            self.load_data()
 
     def load_data(self):
-        # Open the file and read its content
+        """
+        Open the file and read its content
+        """
         with open("src/power_system_simulation/input_network_data.json") as fp:
             # Assign the file content to the data attribute
             self.data = fp.read()
 
     def process_data(self):
+        """
+        Do the processing of the data here.
+        """
         pprint.pprint(json.loads(self.data))
         dataset = json_deserialize(self.data)
         print("components:", dataset.keys())
@@ -33,9 +48,3 @@ class PowerFlow:
 
         serialized_output = json_serialize(output)
         print(serialized_output)
-
-# Instantiate the class
-power_flow_instance = PowerFlow()
-
-# Call the method to process the data
-power_flow_instance.process_data()
---
+++ tests/test_power_flow.py
@@ -0,0 +1,26 @@
+# test_power_flow.py
+
+import json
+import unittest
+
+from power_system_simulation.power_flow_processing import PowerFlow
+
+# test_power_flow.py
+
+
+class TestPowerFlow(unittest.TestCase):
+
+    def test_process_data(self):
+        # Load data from input_network_data.json
+        with open("src/power_system_simulation/input_network_data.json") as file:
+            test_data = file.read()
+
+        # Instantiate the PowerFlow class with test data
+        power_flow_instance = PowerFlow(data=test_data)
+
+        # Call the method to process the data
+        power_flow_instance.process_data()
+
+
+if __name__ == "__main__":
+    unittest.main()
