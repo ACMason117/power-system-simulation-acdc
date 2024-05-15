1504afc0891f7d0af50618066497e8a57aca8578
gijsvanderburgh <165763444+gijsvanderburgh@users.noreply.github.com>
2024-05-15 15:02:29 +0200

    Update power_flow_processing.py

--- src/power_system_simulation/power_flow_processing.py
+++ src/power_system_simulation/power_flow_processing.py
@@ -1,20 +1,41 @@
 import json
 import pprint
 import warnings
-
-with warnings.catch_warnings(action="ignore", category=DeprecationWarning):
-    # suppress warning about pyarrow as future required dependency
-    from pandas import DataFrame
-
+from pandas import DataFrame
 from power_grid_model import PowerGridModel
 from power_grid_model.utils import json_deserialize, json_serialize
 
-with open("src\power_system_simulation\input_network_data.json") as fp:
-    data = fp.read()
+class PowerFlow:
+    """
+    General documentation of this class.
+    You need to describe the purpose of this class and the functions in it.
+    We are using an undirected graph in the processor.
+    """
+    def __init__(self):
+        # Load data upon instantiation
+        self.load_data()
 
-pprint.pprint(json.loads(data))
+    def load_data(self):
+        # Open the file and read its content
+        with open("src/power_system_simulation/input_network_data.json") as fp:
+            # Assign the file content to the data attribute
+            self.data = fp.read()
 
-dataset = json_deserialize(data)
+    def process_data(self):
+        pprint.pprint(json.loads(self.data))
+        dataset = json_deserialize(self.data)
+        print("components:", dataset.keys())
+        print(DataFrame(dataset["node"]))
 
-print("components:", dataset.keys())
-im.show(DataFrame(dataset["node"]))
+        model = PowerGridModel(dataset)
+        output = model.calculate_power_flow()
+        print(DataFrame(output["node"]))
+
+        serialized_output = json_serialize(output)
+        print(serialized_output)
+
+# Instantiate the class
+power_flow_instance = PowerFlow()
+
+# Call the method to process the data
+power_flow_instance.process_data()
