2467fa481e7fc22579d5e7f95786f96644ca98cc
MakiVr <137206673+MakiVr@users.noreply.github.com>
2024-05-29 09:43:26 +0200

    .

---
+++ src/power_system_simulation/Input_data_validity_check.py
@@ -0,0 +1,72 @@
+import json
+import pprint
+import warnings
+
+import pandas as pd
+import power_grid_model as pgm
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+with warnings.catch_warnings(action="ignore", category=DeprecationWarning):
+    # suppress warning about pyarrow as future required dependency
+    from pandas import DataFrame
+
+from power_grid_model import CalculationMethod, CalculationType, PowerGridModel, initialize_array
+from power_grid_model.utils import json_deserialize, json_serialize
+from power_grid_model.validation import assert_valid_input_data
+from pyarrow import table
+
+from power_system_simulation.power_flow_processing import PowerFlow
+from power_system_simulation.graph_processing import GraphProcessor
+
+class NotExactlyOneSourceError(Exception):
+    """Raises MoreThanOneSourceError if there is not exactly one source
+
+    Args:
+        Exception: _description_
+    """
+
+class NotExactlyOneTransformerError(Exception):
+    """Raises MoreThanOneSourceError if there is not exactly one transformer
+
+    Args:
+        Exception: _description_
+    """
+
+class InvalidLVFeederIDError(Exception):
+    """Raises InvalidLVFeederIDError if the LV feeder IDs are not valid line IDS
+
+    Args:
+        Exception: _description_
+    """
+
+class validity_check:
+    
+    def __init__(
+        self, grid_data: dict = None, lv_feeders: list = None, active_power_profile: table = None, reactive_power_profile: table = None, EV_pool: table = None
+    ) -> None:
+        
+        assert_valid_input_data(input_data=grid_data, symmetric=True, calculation_type=CalculationType.power_flow)
+
+        self.grid_data=grid_data
+        self.lv_feeders=lv_feeders
+        self.active_power_profile = active_power_profile
+        self.reactive_power_profile = reactive_power_profile
+        self.EV_pool=EV_pool
+
+        # Check if there is exactly one source
+        if len(grid_data['source']) != 1:
+            raise NotExactlyOneSourceError("There is not exactly one source")
+        
+        # Check if there is exactly one transformer
+        if len(grid_data['transformer']) != 1:
+            raise NotExactlyOneTransformerError("There is not exactly one transformer")
+        
+        # Check if the LV feeder IDs are valid line IDs
+        if lv_feeders not in grid_data['line']['id']:
+            raise InvalidLVFeederIDError("LV feeder IDs are not valid line IDs")
+        
+        # Check if the lines in the LV Feeder IDs have the from_node the same as the to_node of the transformer
+        for 
+
+        
