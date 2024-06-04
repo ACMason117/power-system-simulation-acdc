71afdaf4be22754629254550c08c3b2fa04a609a
gijsvanderburgh <165763444+gijsvanderburgh@users.noreply.github.com>
2024-06-04 10:24:10 +0200

    Added to-do list

--- src/power_system_simulation/EV_penetration_lvl.py
+++ src/power_system_simulation/EV_penetration_lvl.py
@@ -8,3 +8,10 @@ PENETRATION_LEVEL = 0.20
 TOTAL_EVS = NUMBER_OF_HOUSES*PENETRATION_LEVEL
 EVS_PER_FEEDER = math.floor(TOTAL_EVS / NUMBER_OF_FEEDERS) # this function rounds down the output to a whole number
 print(EVS_PER_FEEDER)
+# these values should be given as an input from another file
+
+# Within a LV feeder, randomly select houses which will have EVs
+
+# For each selected house with EV, randomly select an EV charging profile to add to the sym_load of that house.
+
+# After assignment of EV profiles, run a time-series power flow as in Assignment 2, return the two aggregation tables.
