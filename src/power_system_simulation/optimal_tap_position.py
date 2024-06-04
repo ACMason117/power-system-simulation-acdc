9d0ddf3b0c392bdd3409f74a874f748929901ebb
BrittElbers <167319376+BrittElbers@users.noreply.github.com>
2024-06-04 12:18:43 +0200

    tap

--- src/power_system_simulation/optimal_tap_position.py
+++ src/power_system_simulation/optimal_tap_position.py
@@ -1,6 +1,6 @@
 # optimal_tap_position.py
 """
-In this file the processing of the power system should be done. Power system can be given in a different test file. 
+In this file the optimal tap position 
 """
 
 import json
@@ -13,6 +13,7 @@ import pandas as pd
 # import power_grid_model as pgm
 import pyarrow as pa
 import pyarrow.parquet as pq
+import scipy as sp
 
 with warnings.catch_warnings(action="ignore", category=DeprecationWarning):
     # suppress warning about pyarrow as future required dependency
@@ -23,16 +24,24 @@ from power_grid_model.utils import json_deserialize, json_serialize
 from power_grid_model.validation import assert_valid_batch_data, assert_valid_input_data
 from pyarrow import table
 
+from power_system_simulation.power_flow_processing import PowerFlow
+
 
 class PowerProfileNotFound(Exception):
+    """Raises error if power profile is not found"""
+
     pass
 
 
 class TimestampMismatch(Exception):
+    """ " Raises error if timestamps of power profiles do no not match"""
+
     pass
 
 
 class LoadIDMismatch(Exception):
+    """ " Raises error if load IDs of power profiles do no not match"""
+
     pass
 
 
@@ -51,97 +60,12 @@ class OptimalTapPosition:
 
         self.grid_data = grid_data
 
-        # do not delete the next two lines!!!!
-        # self.active_power_profile = active_power_profile
-        # self.reactive_power_profile = reactive_power_profile
-
         self.model = PowerGridModel(self.grid_data)
-
-    def batch_powerflow(self, active_power_profile1: pd.DataFrame, reactive_power_profile1: pd.DataFrame) -> dict:
-        """
-        Create a batch update dataset and calculate power flow.
-
-        Args:
-            active_power_profile1: DataFrame with columns ['Timestamp', '8', '9', '10', ...]
-            reactive_power_profile1: DataFrame with columns ['Timestamp', '8', '9', '10', ...]
-
-        Returns:
-            pd.DataFrame: Power flow solution data.
-        """
-        # check if any power profile is provided
-        if active_power_profile1 is None:
-            raise PowerProfileNotFound("No active power profile provided.")
-
-        if reactive_power_profile1 is None:
-            raise PowerProfileNotFound("No reactive power profile provided.")
-
-        # check if timestamps are equal in value and lengths
-        if active_power_profile1.index.to_list() != reactive_power_profile1.index.to_list():
-            raise TimestampMismatch("Timestamps of active and reactive power profiles do not match.")
-
-        if active_power_profile1.columns.to_list() != reactive_power_profile1.columns.to_list():
-            raise LoadIDMismatch("Load IDs in given power profiles do not match")
-
-        load_profile = initialize_array(
-            "update",
-            "sym_load",
-            (len(active_power_profile1.index.to_list()), len(active_power_profile1.columns.to_list())),
-        )
-
-        load_profile["id"] = active_power_profile1.columns.tolist()
-        load_profile["p_specified"] = active_power_profile1.values.tolist()
-        load_profile["q_specified"] = reactive_power_profile1.values.tolist()
-
-        # Construct the update data
-        update_data = {"sym_load": load_profile}
-
-        # Validate batch data
-        assert_valid_batch_data(
-            input_data=self.grid_data, update_data=update_data, calculation_type=CalculationType.power_flow
-        )
-
-        # Run Newton-Raphson power flow
-        output_data = self.model.calculate_power_flow(
-            update_data=update_data, calculation_method=CalculationMethod.newton_raphson
-        )
-
-        return output_data
-
-    def aggregate_voltage_table(
-        self, active_power_profile1: pd.DataFrame, reactive_power_profile1: pd.DataFrame
-    ) -> pd.DataFrame:
-        """
-        Aggregate power flow results into a table with voltage information.
-
-        Args:
-            output_data (pd.DataFrame): Output data from power flow calculation.
-
-        Returns:
-            pd.DataFrame: Table with voltage information.
-        """
-
-        output_data = self.batch_powerflow(
-            active_power_profile1=active_power_profile1, reactive_power_profile1=reactive_power_profile1
-        )
-
-        voltage_table = pd.DataFrame()
-
-        voltage_table["Timestamp"] = active_power_profile1.index.tolist()
-        voltage_table["max_id"] = output_data["node"][
-            :, pd.DataFrame(output_data["node"]["u_pu"][:, :]).idxmax(axis=1).tolist()
-        ]["id"][0, :]
-        voltage_table["u_pu_max"] = pd.DataFrame(output_data["node"]["u_pu"][:, :]).max(axis=1).tolist()
-        voltage_table["min_id"] = output_data["node"][
-            :, pd.DataFrame(output_data["node"]["u_pu"][:, :]).idxmin(axis=1).tolist()
-        ]["id"][0, :]
-        voltage_table["u_pu_min"] = pd.DataFrame(output_data["node"]["u_pu"][:, :]).min(axis=1).tolist()
-
-        return voltage_table
 
     def tap_position(self, active_power_profile1: pd.DataFrame, reactive_power_profile1: pd.DataFrame) -> pd.DataFrame:
 
-        output_data = self.batch_powerflow(
-            active_power_profile1=active_power_profile1, reactive_power_profile1=reactive_power_profile1
+        output_data = PowerFlow.batch_powerflow(
+            active_power_profile=active_power_profile1, reactive_power_profile=reactive_power_profile1
         )
 
         a = {}
@@ -164,18 +88,18 @@ class OptimalTapPosition:
     ) -> pd.DataFrame:
         tap = 0
 
-        voltage_table = self.aggregate_voltage_table(
-            active_power_profile1=active_power_profile1, reactive_power_profile1=reactive_power_profile1
+        voltage_table = PowerFlow.aggregate_voltage_table(
+            active_power_profile=active_power_profile1, reactive_power_profile=reactive_power_profile1
         )
 
         for i in range(len(voltage_table)):
-            u_pu_max = voltage_table["u_pu_max"][i]
-            u_pu_min = voltage_table["u_pu_min"][i]
+            u_pu_max = voltage_table["Max_Voltage"][i]
+            u_pu_min = voltage_table["Min_Voltage"][i]
             tap_procent_max = (u_pu_max - 1) / 1 * 100
             tap_procent_min = (u_pu_min - 1) / 1 * 100
             tap_max_min = (tap_procent_max + tap_procent_min) / 2
             tap = tap_max_min + tap
 
-        tap_value = tap / (len(voltage_table))
+        tap_value_voltage = tap / (len(voltage_table))
 
-        return tap_value
+        return tap_value_voltage
--- tests/test_optimal_tap_position.py
+++ tests/test_optimal_tap_position.py
@@ -4,19 +4,20 @@ import pandas as pd
 import pytest
 from power_grid_model.utils import json_deserialize_from_file
 
+# Import the PowerFlow class here to avoid circular import
+from power_system_simulation.optimal_tap_position import OptimalTapPosition
+
 
 class TestOptimalTapPosition(unittest.TestCase):
 
     def test_process_data(self):
-        # Import the PowerFlow class here to avoid circular import
-        from power_system_simulation.optimal_tap_position import OptimalTapPosition
 
         # Load data from input_network_data1.json
-        grid_data = json_deserialize_from_file("src/power_system_simulation/input_network_data.json")
+        self.grid_data = json_deserialize_from_file("src/power_system_simulation/input_network_data1.json")
 
         # Load the Active Power Profile file
         try:
-            active_power_profile1 = pd.read_parquet("src/power_system_simulation/active_power_profile1.parquet")
+            self.active_power_profile1 = pd.read_parquet("src/power_system_simulation/active_power_profile1.parquet")
         except FileNotFoundError:
             self.fail(
                 "Active Power Profile file not found. Please ensure 'src/power_system_simulation/active_power_profile1.parquet' is in the correct location."
@@ -25,7 +26,9 @@ class TestOptimalTapPosition(unittest.TestCase):
 
         # Load the Reactive Power Profile file
         try:
-            reactive_power_profile1 = pd.read_parquet("src/power_system_simulation/reactive_power_profile1.parquet")
+            self.reactive_power_profile1 = pd.read_parquet(
+                "src/power_system_simulation/reactive_power_profile1.parquet"
+            )
         except FileNotFoundError:
             self.fail(
                 "Reactive Power Profile file not found. Please ensure 'src/power_system_simulation/reactive_power_profile1.parquet' is in the correct location."
@@ -33,7 +36,7 @@ class TestOptimalTapPosition(unittest.TestCase):
             return
 
         # Instantiate the optimal_tap_position_instance class with test data and power profiles
-        optimal_tap_position_instance = OptimalTapPosition(grid_data=grid_data)
+        optimal_tap_position_instance = OptimalTapPosition(grid_data=self.grid_data)
 
 
 if __name__ == "__main__":
