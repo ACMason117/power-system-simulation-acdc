7d41f7eb2b0b3345738aa08d5201d070e3df5aed
BrittElbers <167319376+BrittElbers@users.noreply.github.com>
2024-06-05 09:54:27 +0200

    Tesr

--- tests/test_optimal_tap_position.py
+++ tests/test_optimal_tap_position.py
@@ -3,6 +3,7 @@ import unittest
 import pandas as pd
 import pytest
 from power_grid_model.utils import json_deserialize_from_file
+import power_system_simulation.optimal_tap_position as otp
 
 # Import the PowerFlow class here to avoid circular import
 from power_system_simulation.optimal_tap_position import OptimalTapPosition
@@ -38,6 +39,11 @@ class TestOptimalTapPosition(unittest.TestCase):
         # Instantiate the optimal_tap_position_instance class with test data and power profiles
         optimal_tap_position_instance = OptimalTapPosition(grid_data=self.grid_data)
 
+        self.pf = otp.PowerFlow(grid_data=self.grid_data)
+
+    def optimal_tap_voltage(self):
+        tap_value_voltage = self.otp.optimal_tap_voltage(self.active_power_profile1, self.reactive_power_profile1)
+        self.assertIsInstance(tap_value_voltage, dict)
 
 if __name__ == "__main__":
     unittest.main()
