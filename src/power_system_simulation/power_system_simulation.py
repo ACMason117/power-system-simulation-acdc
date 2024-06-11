821d302ae0d648d764d67a0084ea59e0f90f95bc
ACMason117 <hasimmelaja@gmail.com>
2024-06-11 11:47:39 +0200

    Main skeleton of assignment 3
    Main skeleton of assignment 3 + where to function skeletons
    
    new.ipynb file created to show how to access functions from assignment 3 and assignment 1/2

---
+++ src/power_system_simulation/power_system_simulation.py
@@ -0,0 +1,23 @@
+
+import power_flow_processing as pfp
+import graph_processing as gp
+
+
+# write exceptions here
+
+class PowerSim:
+    def __init__(self, grid_data: dict) -> None:
+        self.PowerSimModel = pfp.PowerFlow(grid_data=grid_data)
+    
+    def example_code(self):
+        print("Who reads trek een bak")
+
+    def n1_calculations():
+        pass
+
+    def ev_penetration():
+        pass
+
+    def optimal_tap_position():
+        pass
+
