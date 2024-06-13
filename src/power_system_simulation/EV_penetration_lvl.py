1fc5c26fcdccbbebdb1eb1a7ab47e22600771401
gijsvanderburgh <165763444+gijsvanderburgh@users.noreply.github.com>
2024-06-13 19:43:01 +0200

    Data transform

--- src/power_system_simulation/EV_penetration_lvl.py
+++ src/power_system_simulation/EV_penetration_lvl.py
@@ -1,7 +1,9 @@
+import os
 import math
-import random
 import pandas as pd
+import graph_processing as tp  
 from power_grid_model.utils import json_deserialize_from_file
+import power_flow_processing as pfp
 
 def process_evs(number_of_houses, number_of_feeders, penetration_level):
     # Calculate the total number of EVs and the number per feeder
@@ -9,29 +11,21 @@ def process_evs(number_of_houses, number_of_feeders, penetration_level):
     evs_per_feeder = math.floor(total_evs / number_of_feeders)
     print(f"EVs per feeder: {evs_per_feeder}")
 
-    grid_data = json_deserialize_from_file("input_network_data.json, engine='pyarrow'")
-    active_power_profile = pd.read_parquet("active_power_profile.parquet, engine='pyarrow'")
-    reactive_power_profile = pd.read_parquet("reactive_power_profile.parquet, engine='pyarrow'")
+    # Determine the file path relative to this script's location
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    grid_data_path = os.path.join(script_dir, "Assigment3_data\input_network_data.json")
+    active_power_profile_path = os.path.join(script_dir, "active_power_profile.parquet")
+    reactive_power_profile_path = os.path.join(script_dir, "reactive_power_profile.parquet")
+    
+    # Import data
+    grid_data = json_deserialize_from_file(grid_data_path)
+    active_power_profile = pd.read_parquet(active_power_profile_path)
+    reactive_power_profile = pd.read_parquet(reactive_power_profile_path)
 
-    # import all the data
-
-    # Find all LV feeder IDs 
-    '''
-    1. Find transformer
-    2. Set all lines connected after transformer as LV feeder
-    '''
-    # Run downstream vertices and fin all sym_loads per LV feeder
-    # Randomly select houses for EV assignment within each feeder, so randomly give a charging profile to each
-
-    # these values should be given as an input from another file
-
+    # Randomly select houses for EV assignment within each feeder
+    # These values should be given as an input from another file
     # Within a LV feeder, randomly select houses which will have EVs
-
     # For each selected house with EV, randomly select an EV charging profile to add to the sym_load of that house.
-
     # After assignment of EV profiles, run a time-series power flow as in Assignment 2, return the two aggregation tables.
-
-    return evs_per_feeder, grid_data
-grid_data = json_deserialize_from_file("input_network_data.json")
-
-print(grid_data)
+    pgm = pfp.PowerFlow(grid_data)
+    return evs_per_feeder
