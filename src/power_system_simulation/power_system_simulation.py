f9bd0029b8ed31691f7fa99cdf7fa00ac31a7354
ACMason117 <hasimmelaja@gmail.com>
2024-06-14 13:53:49 +0200

    Update power_system_simulation.py
    finding the error, part 1

--- src/power_system_simulation/power_system_simulation.py
+++ src/power_system_simulation/power_system_simulation.py
@@ -54,8 +54,6 @@ class PowerSim:
     def __init__(self, grid_data: dict, lv_feeders: list = None) -> None:
         self.PowerSimModel = pfp.PowerFlow(grid_data=grid_data)
 
-        from power_system_simulation.graph_processing import GraphProcessor
-
         # assert_valid_input_data(input_data=grid_data, symmetric=True, calculation_type=CalculationType.power_flow)
 
         self.grid_data = grid_data
@@ -103,20 +101,9 @@ class PowerSim:
         source_vertex_id = grid_data["source"]["node"][0]
         edge_ids = list(grid_data["line"]["id"]) + list(grid_data["transformer"]["id"])
         vertex_ids = grid_data["node"]["id"]
-        vertex_visited = []
-        vertex_parents = {}
-        adjacency_list = GraphProcessor.build_adjacency_list(self, edge_vertex_id_pairs, edge_enabled)
+        
+        
         self.graph = gp.GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
-        if (
-            GraphProcessor.dfs(self, adjacency_list, vertex_visited, float("Nan"), vertex_parents, source_vertex_id)
-            == 1
-        ):
-            raise gp.GraphCycleError("Cycle found")
-
-        # 7. The graph should be fully connected
-        if len(vertex_visited) != len(vertex_ids):
-            raise gp.GraphNotFullyConnectedError("Graph not fully connected. Cannot reach all vertices.")
-        assert_valid_input_data(input_data=grid_data, symmetric=True, calculation_type=CalculationType.power_flow)
 
     def example_code(self):
         print("Who reads trek een bak")
